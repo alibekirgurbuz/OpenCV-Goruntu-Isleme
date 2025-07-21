f9a225f7b74f819875b0ea49d435273dcafa26d1
Ali Gürbüz <alibekirgurbuz@posta.mu.edu.tr>
2025-07-21 14:59:28 +0300

    v1.5

---
+++ Piksel_Islemleri/PikseldenBinarye.py
@@ -0,0 +1,8 @@
+import cv2
+
+img = cv2.imread("../Djital_Goruntu_Nedir/pp.jpg")
+y, x = 100, 200
+b, g, r = img[y, x]
+print(f"B: {b} ({format(b, '08b')})") ## 8-bit binary olarak formatla
+print(f"G: {g} ({format(g, '08b')})")
+print(f"R: {r} ({format(r, '08b')})")
