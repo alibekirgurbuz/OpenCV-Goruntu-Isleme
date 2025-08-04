12803b665dafbf41d30cd2677dbe14025c0fb516
Ali Gürbüz <alibekirgurbuz@posta.mu.edu.tr>
2025-08-04 16:36:27 +0300

    v2.4

--- 17_Otopark_Bos_Alan_Tespiti/Otopark.py
+++ 17_Otopark_Bos_Alan_Tespiti/Otopark.py
@@ -1,5 +1,9 @@
 import cv2
 import pickle
+# Otopark park alanı tespiti için nokta ekleme ve silme uygulaması
+
+# Bu try except bloğu önceden kaydedilmiş nokta listesini yükler.
+# Eğer dosya yoksa, boş bir liste oluşturur.
 try:
     with open ("noktalar", "rb") as f:
         liste = pickle.load(f)
@@ -8,13 +12,13 @@ except:
 
 def mouse(events, x, y, flags, params):
     if events == cv2.EVENT_LBUTTONDOWN:
-        liste.append((x, y))
+        liste.append((x, y)) # Sol tıkla (x, y) koordinatlarını listeye ekler
 
     if events == cv2.EVENT_RBUTTONDOWN:
         for i, pos in enumerate(liste):
             x1, y1 = pos
             if x1<x<x1 + 26 and y1<y<y1 + 15:
-                liste.pop(i)
+                liste.pop(i) # Tıklanan konumda 26x15 piksel dikdörtgen içinde işaretleyici varsa siler
 
     with open("noktalar", "wb") as f:
         pickle.dump(liste, f)
@@ -24,7 +28,7 @@ while True:
     img = cv2.imread("../Assets/first_frame.png")
 
     for I in liste:
-        cv2.rectangle(img, I, ( I[0]+26, I[1]+15), (255,0, 0), 2)
+        cv2.rectangle(img, I, ( I[0]+26, I[1]+15), (255,0, 0), 2) # Liste içindeki her nokta için 26x15 piksel boyutunda dikdörtgen çizer
 
 
     cv2.imshow("img", img)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-OpenCV ile Görüntü İşleme
+## OpenCV ile Görüntü İşleme
+
+
+##### Proje içerisinde kullanılan tüm Python kütüphanelerini yüklemek için aşağıdaki komutu çalıştırın:
+
+`python -m pip install -r requirements.txt`
